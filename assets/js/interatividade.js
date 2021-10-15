ea48dd0e2dc1dd9698c65c040b7a4cd6cd0a5804
Caiane Hardt <caianehardt@gmail.com>
2021-10-14 21:36:41 -0300

    adicionando efeito scroll via JS

--- assets/js/interatividade.js
+++ assets/js/interatividade.js
@@ -1,56 +1,14 @@
-function goTo(id){
-    var obj = eval("colors."+id);
+const sr = ScrollReveal({
+    origin: "bottom",
+    distance: "50px",
+    duration: 2000,
+    reset: true,
+  });
 
-    $("body").css("background",obj.background);
-    $("ul.slimmenu li a").css("color",obj.background);
-    if(obj.index > currentIndex){
-        $(".active").addClass("off");
-        $(".active").transition({
-            x : -100,
-            opacity : 0,
-            zIndex : 0
-        },600);
+sr.reveal("#apresentacaoFoto", { delay: 200 });
+sr.reveal("#apresentacaoTexto", { delay: 400 });
 
-        $("#"+currentId).removeClass("active");
+sr.reveal("#quem-sou", { delay: 600 });
+sr.reveal(".conhecimentos", { delay: 400 });
 
-        $("#"+id).addClass("active");
-        $("#"+id).transition({
-            perspective: '100px',
-            rotate3d: '1,1,0,90deg'
-        },0,function(){
-            $("#"+id).removeClass("off");
-            $("#"+id).transition({
-                rotate3d: '0,0,0,0deg',
-            x : 0,
-                opacity : 1,
-                zIndex : 2
-            },600);
-        });
-    }else if(obj.index < currentIndex){
-        $(".active").addClass("off");
-        $(".active").transition({
-            x : 100,
-            opacity : 0,
-            zIndex : 0
-        },600);
-        $("#"+currentId).removeClass("active");
-
-
-        $("#"+id).addClass("active");
-        $("#"+id).transition({
-            perspective: '100px',
-            rotate3d: '1,1,0,-90deg'
-        },0,function(){
-            $("#"+id).removeClass("off");
-            $("#"+id).transition({
-                rotate3d: '0,0,0,0deg',
-            x : 0,
-                opacity : 1,
-                zIndex : 2
-            },600);
-        });
-    }
-    currentIndex = obj.index;
-    currentId = id;
-
-}
+sr.reveal(".contatos", { delay: 400 });
