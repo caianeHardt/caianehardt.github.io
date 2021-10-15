95b331bf6fef458fb4e511f34d6c3f409691340c
Caiane Hardt <caianehardt@gmail.com>
2021-10-14 21:39:06 -0300

    bugfix scroll

--- assets/js/interatividade.js
+++ assets/js/interatividade.js
@@ -8,7 +8,7 @@ const sr = ScrollReveal({
 sr.reveal("#apresentacaoFoto", { delay: 200 });
 sr.reveal("#apresentacaoTexto", { delay: 400 });
 
-sr.reveal("#quem-sou", { delay: 600 });
+sr.reveal("#quem-sou", { delay: 400 });
 sr.reveal(".conhecimentos", { delay: 400 });
 
 sr.reveal(".contatos", { delay: 400 });
